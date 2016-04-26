31ce7265f8e7ada8e435e80bc4a64e954f42e10e
xuefeng Huang <timhxf@gmail.com>
2016-04-26 08:57:10 +0000

    add score for each writer

--- job_match/services.py
+++ job_match/services.py
@@ -1,10 +1,12 @@
 from models import Writer
 from newspaper import Article # extract the main content
 import rake # extract keywords
+import os
+from django_contect_matching.settings import BASE_DIR
 
 def get_writer_score(writer):
     industry_list = writer.industry.split(',')
-    if any(item in ['Lifestyle & Travel'] for item in industry_list):
+    if any(item.strip() in ['Lifestyle & Travel'] for item in industry_list):
         total_links = 0
         relavent_links = 0
         publications = writer.publication_set.all()
@@ -12,7 +14,16 @@ def get_writer_score(writer):
         if publications:
             for item in publications:
                 article_text = get_text(item.link)
-                article_keywords = get_keywords(article_text)
+                article_keywords = get_keywords(article_text) # list of tuples ('key words', score)
+                for keywords in article_keywords:
+                    print keywords[0]
+                    items = keywords[0].split() # get the keywords part and split into a list
+                    if any(item in ['travel', 'singapore', 'tourism', 'sightseeing'] for item in items):
+                        relavent_links += 1
+                        break
+                total_links += 1
+                
+            return float(relavent_links) / total_links * min(relavent_links, 100)
         else:
             return 0
     else:
@@ -25,5 +36,7 @@ def get_text(link):
     return article.text
     
 def get_keywords(text):
-    rake_obj = rake.Rake('stop_words.txt', 1, 2, 2)
+    rake_obj = rake.Rake(os.path.join(BASE_DIR, 'job_match', 'stop_words.txt'), 3, # at least xxx char in each keyword
+                                                                                5, # at most xxx words in the phrase
+                                                                                2) # at least appear xxx times
     return rake_obj.run(text)[0:3] # return first 3 keywords list
--- job_match/views.py
+++ job_match/views.py
@@ -1,10 +1,22 @@
 #coding:utf-8
 from django.shortcuts import render
+from services import get_writer_score
+from models import Writer
 
 
 # score = 赞成比例 * min(投票数, 100)
 
 def index(req):
     if req.method == 'POST':
-        
+        writer_list = Writer.objects.all()
+        for writer in writer_list:
+            writer.score = get_writer_score(writer)
+            print writer.score
+            
+        # sort the list
+        writer_list = list(writer_list)
+        writer_list.sort(key=lambda writer : writer.score, reverse=True)
+        for writer in writer_list:
+            print writer.name, writer.score
+        return render(req, 'match_writer.html', {'writer_list' : writer_list})
     return render(req, 'index.html', {})
